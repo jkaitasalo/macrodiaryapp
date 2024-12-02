2ca354cbb9d5d9d0cd5afc2813df894cb3f46bdc
jkaitasalo <jouni.kaitasalo@hotmail.com>
2024-12-02 16:52:22 +0200

    commit

---
+++ src/main/java/s24/macroapp/MacroappApplication.java
@@ -0,0 +1,13 @@
+package s24.macroapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacroappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacroappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/s24/macroapp/MacroappApplicationTests.java
@@ -0,0 +1,13 @@
+package s24.macroapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacroappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
